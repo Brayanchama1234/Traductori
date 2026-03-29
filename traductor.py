630603356c9221c4f2f4bc9add1ace4008fb738a
Brayanchama1234 <brayanchamacarios@gmail.com>
2026-03-29 06:20:20 -0400

    Delete traductor.py

--- traductor.py
+++
@@ -1,6 +0,0 @@
-Una herramienta que  permite traducir hasta 7 Idiomas al Español y Viceversa los textos que necesites comprender y saber su significado.
-
-
-git clone https://github.com/Brayanchama1234/Traductori 
-cd Traductor 
-python traductor.py
